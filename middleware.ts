98221452aee2bb5bbeb1043e87180ea0bdd62334
Jonathan Cai <caimjonathan@gmail.com>
2023-01-26 23:09:06 -0500

    feat: middleware vercel.pub => tokengate.xyz

--- middleware.ts
+++ middleware.ts
@@ -34,7 +34,7 @@ export default async function middleware(req: NextRequest) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.pub`, "")
+          .replace(`.tokengate.xyz`, "")
           .replace(`.shopify-aave.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
 
