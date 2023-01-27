8c616fd899fe40653d56bf8565aefeef34248a8f
Jonathan Cai <caimjonathan@gmail.com>
2023-01-26 22:23:58 -0500

    feat: middleware direct to /

--- middleware.ts
+++ middleware.ts
@@ -22,20 +22,20 @@ export default async function middleware(req: NextRequest) {
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
 
-  // Only for demo purposes - remove this if you want to use your root domain as the landing page
-  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://demo.vercel.pub");
-  }
+  // // Only for demo purposes - remove this if you want to use your root domain as the landing page
+  // if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
+  //   return NextResponse.redirect("https://demo.vercel.pub");
+  // }
 
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
-      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
+      in this case, our team slug is "platformize", thus *.shopify-aave.vercel.app works. Do note that you'll
+      still need to add "*.shopify-aave.vercel.app" as a wildcard domain on your Vercel dashboard. */
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
           .replace(`.vercel.pub`, "")
-          .replace(`.platformize.vercel.app`, "")
+          .replace(`.shopify-aave.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
 
   // // rewrites for app pages
@@ -54,10 +54,10 @@ export default async function middleware(req: NextRequest) {
   // }
   console.log({ currentHost });
 
-  // // rewrite root application to `/home` folder
-  // if (hostname === "localhost:3000" || hostname === "platformize.vercel.app") {
-  //   return NextResponse.rewrite(new URL(`/home${path}`, req.url));
-  // }
+  // rewrite root application to `/` folder
+  if (hostname === "localhost:3000" || hostname === "shopify-aave.vercel.app") {
+    return NextResponse.rewrite(new URL(`/${path}`, req.url));
+  }
 
   // rewrite everything else to `/_sites/[site] dynamic route
   return NextResponse.rewrite(
