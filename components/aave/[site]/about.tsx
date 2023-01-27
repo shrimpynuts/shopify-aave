d57e5f1aacd2427d0e5e1967f7eae34d20e567de
Jonathan Cai <caimjonathan@gmail.com>
2023-01-27 03:30:34 -0500

    feat: aave/about page demo contract call

--- components/aave/[site]/about.tsx
+++ components/aave/[site]/about.tsx
@@ -1,15 +1,21 @@
 import Account from "@/components/shared/account";
 import { ISiteProps } from "@/pages/_sites/[site]";
+import RegisterDomain from "../RegisterDomain";
 
 export default function AboutSite({ data: { owner } }: { data: ISiteProps }) {
   return (
     <div className="relative w-full px-32">
-      <div className="mx-auto max-w-lg rounded border border-gray-200 bg-white p-8 shadow-md">
+      <div className="mx-auto max-w-2xl rounded border border-gray-200 bg-white p-8 shadow-md">
         <p className="mb-4">
           This demonstrates that the data is dynamic, even on routes like
           /about.
         </p>
         <Account address={owner} />
+        <p className="my-4">
+          This demonstrates that you can interact with smart contracts on
+          subdomains.
+        </p>
+        <RegisterDomain />
       </div>
     </div>
   );
